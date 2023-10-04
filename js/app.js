1de4dc27feb3b9c7cf2e85634e491ea7110c1bb2
Lytween <victoire.lytween@gmail.com>
2023-10-04 22:33:29 +0200

    04/10/23

--- js/app.js
+++ js/app.js
@@ -25,3 +25,21 @@ function onScroll(){
 }
 
 window.addEventListener('scroll',onScroll);
+
+// header animation
+
+const cursor = document.querySelector(".cursor");
+const frontend = document.querySelector(".dev");
+const title = document.querySelector("#title");
+
+const hero = document.querySelector('[data-hero]')
+
+window.addEventListener('mousemove', (e) => {
+	const { clientX, clientY } = e
+  const x = Math.round((clientX / window.innerWidth) * 90)
+  const y = Math.round((clientY / window.innerHeight) * 90)
+	
+	hero.style.setProperty('--x', `${x}%`)
+	hero.style.setProperty('--y', `${y}%`)
+  
+})
